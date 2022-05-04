0ea8780af487f748701fd8c3416143176fc943eb
João Victor Davi <j.victordavid2@gmail.com>
2022-05-04 00:12:43 -0300

    chore: add tailwind forms and scrollbar plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257E5'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
